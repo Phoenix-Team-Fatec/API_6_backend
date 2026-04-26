48054365b99dab261feda8537756d12b6bb7d42e
Joniel R. de Oliveira <jonielrodriguesdeoliveira@gmail.com>
2026-04-26 16:42:00 -0300

    :sparkles: feat(#11): add Brand model with fields and MongoDB annotations

---
+++ src/main/java/team/phoenix/backend/domain/model/Brand.java
@@ -0,0 +1,31 @@
+package team.phoenix.backend.domain.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+@Document(collection = "brands")
+@CompoundIndex(name = "codigo_unique_idx", def = "{'codigo': 1}", unique = true)
+public class Brand {
+    @Id private String id;
+    private Integer codigo;
+    private String nome;
+    private String descricao;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Brand(Integer codigo, String nome, String descricao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.createdAt = LocalDateTime.now();
+    }
+}
